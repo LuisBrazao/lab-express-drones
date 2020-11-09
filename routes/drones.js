99ee13d47e248a98e04a37ae6e59539cb1413fea
Luis Brazao <luis.ms.brazao@gmail.com>
2020-11-09 17:05:54 +0000

    lab done, TODO bonus (style)

--- routes/drones.js
+++ routes/drones.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Drone = require("../models/Drone.models")
 
 // require the Drone model here
 
@@ -6,31 +7,65 @@ const router = express.Router();
 
 router.get('/drones', (req, res, next) => {
   // Iteration #2: List the drones
+  Drone.find()
+  .then((allTheDroneFromDB) => {
+    res.render("drones/list", { drones: allTheDroneFromDB });
+  })
   // ... your code here
 });
 
 router.get('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
+  res.render("drones/create-form")
   // ... your code here
 });
 
 router.post('/drones/create', (req, res, next) => {
   // Iteration #3: Add a new drone
+  let { name, propellers, maxSpeed } = req.body
+  Drone.create({
+    name: name,
+    propellers: propellers,
+    maxSpeed: maxSpeed
+  }).then(() => {
+    res.redirect("/drones")
+  })
   // ... your code here
 });
 
 router.get('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
+  let droneID = req.params.id;
+  Drone.findById(droneID)
+    .then(theDrone => res.render('drones/update-form', { drone: theDrone }))
+    .catch(error => res.render("error", { error }))
   // ... your code here
 });
 
 router.post('/drones/:id/edit', (req, res, next) => {
   // Iteration #4: Update the drone
+  let droneID = req.params.id;
+  let { name, propellers, maxSpeed } = req.body;
+  Drone.findByIdAndUpdate(droneID, {
+    name,
+    propellers,
+    maxSpeed,  
+  })
+    .then(() => {
+      res.redirect("/drones")
+    })
+    .catch(error => res.render("error", { error }))
   // ... your code here
 });
 
 router.post('/drones/:id/delete', (req, res, next) => {
   // Iteration #5: Delete the drone
+  let droneID = req.params.id;
+  Drone.findByIdAndRemove(droneID)
+    .then(() => {
+      res.redirect("/drones")
+    })
+    .catch(error => res.render("error", { error }))
   // ... your code here
 });
 
